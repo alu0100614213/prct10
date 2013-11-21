e1b034ecd8d26bacaf08d1d428ff8ec0a89a0615
Sebastian <alu0100614213@ull.edu.es>
2013-11-21 23:05:31 +0000

    Añadiendo la clase MatrizDensa al fichero matriz_sf.rb de la carpeta lib.

--- lib/matriz_sf.rb
+++ lib/matriz_sf.rb
@@ -12,8 +12,8 @@ module Matriz_sf
 	
 	##Declarar metodos abstactos
     end
-
-  class Vectordisperso
+	
+  class VectorDisperso
     attr_reader :vector
     
     def initialize(h = {})
@@ -30,7 +30,7 @@ module Matriz_sf
     end
   end
 
-  class Matrizdispersa
+  class MatrizDispersa
     attr_reader :matrix
     
     def initialize(h = {})
@@ -62,6 +62,80 @@ module Matriz_sf
       str = "{ #{str} }"
     end
   end
+
+	class MatrizDensa
+	  
+	  def initialize (matrix, tam = matrix[0].size)
+	    @matrix = matrix
+	    @fila = matrix.size
+	    @columna = tam
+	  end
+	  
+	  attr_reader :matrix, :fila, :columna
+	    
+	  def mostrar #Funcion mostrar matriz
+	    x = y = 0 # inicializamos las variables
+	    puts "matriz: " 
+	    while x < fila # primer bucle
+	      while y < columna # segundo bucle
+		print("#{matrix[x][y]}") #imprimimos en la posicion en la que se encuentre
+		y += 1
+	      end
+	      puts
+	      x += 1
+	      y = 0
+	    end
+	  end
+	  
+	  def to_s #Funcion mostrar matriz
+	    cadena = ""
+	    x = y = 0 
+	    while x < fila # primer bucle
+	      while y < columna # segundo bucle
+		cadena += "#{matrix[x][y]} "
+		y += 1
+	      end
+	      cadena +="\n"
+	      x += 1
+	      y = 0
+	    end
+	    cadena
+	  end
+	  
+	    
+	  def +(o)
+	    raise unless (o.is_a? Matriz) and (fila == o.fila) and (columna == o.columna)
+	      mat = []
+	      x = y = 0
+	      while x < fila
+		while y < columna
+		  if y == 0
+		    mat << [matrix[x][y] + o.matrix[x][y]]
+		  else
+		    mat[x] << (matrix[x][y] + o.matrix[x][y])
+		  end
+		  y += 1
+		end
+		x += 1
+		y = 0
+	      end
+	      Matriz.new(mat)
+	  end
+	  
+	  def *(o)
+	    raise unless (o.is_a? Matriz) and (columna == o.fila) 
+	    mat = Array.new(fila) { Array.new(o.columna) }
+	      for i in 0...@fila do
+		for j in 0...o.columna do
+		  mat[i][j]=Fraccion.new(0,1);
+		  for k in 0...o.fila do
+		    mat[i][j] += matrix[i][k] * o.matrix[k][j]
+		  end
+		end
+	      end
+	      Matriz.new(mat)
+	  end
+	end 
 
   class Fraccion  #Clase Fraccion
     include Comparable
