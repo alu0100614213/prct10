dc4bb17c11400b3b893da171d7a984b7994fdaa7
Fabio Mendoza Bello <alu0100463910@ull.edu.es>
2013-11-22 00:20:17 +0000

    Arreglo  de pruebas en el spec

--- spec/matriz_sf_spec.rb
+++ spec/matriz_sf_spec.rb
@@ -1,34 +1,25 @@
-require "Matriz.rb"
+require "matriz_sf.rb"
+include Matriz_sf
 
+describe Matriz_sf do
+  
+   describe Matriz do
+        before :all do
+            MM = Matriz.new
+        end
+        
+        describe "Basic" do
+            it 'Existe una clase abstracta Matriz?' do
+                MM.instance_of?(Matriz) == true
+            end
+            it 'se puede acceder a los atributos (fila)?' do
+                MM.fila.should == 0
+            end
 
-describe Matriz do
-  
-  before :each do
-    @m1 = Matriz.new([[1,2,3],[4,5,6]])
-    @m2 = Matriz.new([[3,3,3],[3,3,3],[3,3,3]])
-    @m3 = Matriz.new([[3,3,3],[3,3,3]])
-    @f1 = Fraccion.new(4,8)
-    @f2 = Fraccion.new(3,2)
-    @f3 = Fraccion.new(5,2)
-    
-    @matriz1 =Matriz.new([[@f1,@f2],[@f3,@f1]])
-       
-    @matriz2 =Matriz.new([[@f2,@f2],[@f2,@f2]])
-  end
-  
-  describe "Existe Metodo to string?" do
-    it { @p1.should respond_to :to_s }
-  end
-  
-  it "Se debe poder sumar dos matrices de enteros" do
-    (@m1+@m3).matrix.inspect.should == "[[4, 5, 6], [7, 8, 9]]"
-    (@matriz1+@matriz2).matrix.inspect.should == "[[2, 3], [4, 2]]"
-  end
-  
-  it "Se debe poder multiplicar una matriz de Enteros" do
-  #  (@m1*@m2).matrix.inspect.should == "[[18, 18, 18], [45, 45, 45]]"
-    (@matriz1*@matriz2).matrix.inspect.should == "[[3, 3], [9/2, 9/2]]"
-  end
-  
-end
-   
+            it 'Existe metodo to_s abstracto?' do
+                MM.should respond_to("to_s")
+            end
+
+        end
+    end
+ end
